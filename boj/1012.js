b9024cd163e53aa6cfce8afd5c748c39d1ebdf84
imb96 <kimminje7267@naver.com>
2024-02-27 17:27:15 +0900

    sol: boj 1012

--- boj/1012.js
+++ boj/1012.js
@@ -1,72 +1,56 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-// let input = `2
-// 10 8 17
-// 0 0
-// 1 0
-// 1 1
-// 4 2
-// 4 3
-// 4 5
-// 2 4
-// 3 4
-// 7 4
-// 8 4
-// 9 4
-// 7 5
-// 8 5
-// 9 5
-// 7 6
-// 8 6
-// 9 6
-// 10 10 1
-// 5 5`.toString().trim().split("\n");
+const T = parseInt(input.shift());
 
-let T = parseInt(input.shift());
-let idx = 0;
-let result = [];
+let start = 0;
+const result = [];
 
-for(let i = 0; i < T; i++) {
-  let [col, row, baechu] = input[idx].split(' ').map(Number);
+for (let i = 0; i < T; i++) {
+  const [M, N, K] = input[start].split(" ").map(Number);
   let count = 0;
-  let farm = Array.from(new Array(row), () => new Array(col));
 
-  for(let j = 0; j < baechu; j++) {
-    let [newCol , newRow] = input[idx + j + 1].split(' ').map(Number);
-    farm[newRow][newCol] = 1;
+  const map = Array.from({ length: N }, () => Array(M).fill(0));
+
+  for (let j = start + 1; j < start + 1 + K; j++) {
+    const [x, y] = input[j].split(" ").map(Number);
+    map[y][x] = 1;
   }
 
-  for(let k = 0; k < row; k++) {
-    for(let l = 0; l < col; l++) {
-      if(farm[k][l] === 1) {
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[0].length; col++) {
+      if (map[row][col] === 1) {
         count++;
-        dfs(k,l,farm,row,col);
+        dfs(map, row, col, M, N);
       }
     }
   }
 
   result.push(count);
-  idx += baechu + 1;
+  start += K + 1;
 }
 
-console.log(result.join('\n'));
+function dfs(map, row, col, M, N) {
+  map[row][col] = -1;
 
-function dfs(row, col, farm, maxRow, maxCol) {
-  farm[row][col] = 'X';
-
-  if(row > 0) {
-    if(farm[row-1][col] === 1) dfs(row-1, col, farm, maxRow, maxCol);
+  if (row > 0) {
+    if (map[row - 1][col] === 1) dfs(map, row - 1, col, M, N);
   }
 
-  if(col > 0) {
-    if(farm[row][col-1] === 1) dfs(row, col-1, farm, maxRow, maxCol);
+  if (col > 0) {
+    if (map[row][col - 1] === 1) dfs(map, row, col - 1, M, N);
   }
 
-  if(row < maxRow-1) {
-    if(farm[row+1][col] === 1) dfs(row+1, col, farm, maxRow, maxCol);
+  if (row < N - 1) {
+    if (map[row + 1][col] === 1) dfs(map, row + 1, col, M, N);
   }
 
-  if(col < maxCol-1) {
-    if(farm[row][col+1] === 1) dfs(row, col+1, farm, maxRow, maxCol);
+  if (col < M - 1) {
+    if (map[row][col + 1] === 1) dfs(map, row, col + 1, M, N);
   }
 }
+
+console.log(result.join("\n"));
