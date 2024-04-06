79221cfb0e0da5ca4123c6c7487720e9ea423c8b
imb96 <kimminje7267@naver.com>
2024-04-06 15:13:21 +0900

    sol: pg 두 개 뽑아서 더하기

---
+++ programmers/algorithm/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  const result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      result.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  const resultSet = new Set(result);
+  return [...resultSet].sort((a, b) => a - b);
+}
